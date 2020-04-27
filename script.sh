93471bb4924c7cb43d1f5011fdfa1656f5b194fa
Gavroche <64252014+G-a-v-r-o-c-h-e@users.noreply.github.com>
2020-04-27 10:02:09 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ mpc clear
 wget -qO- http://podcasts.joerogan.net/ > .source$
 cat .source$ | grep -Eoi 'data-stream-url=[^>]+ data' | grep -Eo 'http://[^>]+"' | tr -d '"' > .url$
 rm .source$
-lastshow=$(sed -n '1p' .url)
+lastshow=$(sed -n '1p' .url$)
 mpc add $lastshow
 mpcplay
 for url in `tail -n +2 .url$`;do mpc add $url;done
